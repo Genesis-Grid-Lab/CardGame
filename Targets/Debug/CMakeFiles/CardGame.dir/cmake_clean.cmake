122909a32eb85f5721b1904c570b82465e964553
Nesmy <pnesmy@gmail.com>
2024-05-01 16:21:07 -0400

    hello world

---
+++ Targets/Debug/CMakeFiles/CardGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardGame.dir/src/main.cpp.o"
+  "CMakeFiles/CardGame.dir/src/main.cpp.o.d"
+  "bin/CardGame"
+  "lib/CardGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CardGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
